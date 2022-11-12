a236035c489ab54fa2a5deb4422b78371ed6f2b9
prabhattopi <topi9864@gmail.com>
2022-11-12 14:54:55 +0530

    The file is update with cofig remove gitignore

---
+++ config/test.ts
@@ -0,0 +1,8 @@
+export default {
+    port :4000,
+    host:"localhost",
+    dbUri:"mongodb+srv://Prabhattopi:prabhat1234@cluster0.tkcx8q3.mongodb.net/merntest?retryWrites=true&w=majority",
+    JWT_SECRET:"abc1234"
+
+
+}
